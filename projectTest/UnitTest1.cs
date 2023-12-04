168bd17775772aa0ce9c0595b9ea3959ac8b13b6
v <ryanyosedieirawan@gmail.com>
2023-12-04 12:37:11 +0700

    starting

---
+++ projectTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace projectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
